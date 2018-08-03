dd2e30eb1d6125c0945d8e08c9e1614ad12644c7
Mateusz Kantyka <mateusz.kantyka@hotmail.com>
2018-08-03 13:00:04 +0200

    add Retrieve Stock Quantity services

--- lib/seko/client.rb
+++ lib/seko/client.rb
@@ -7,8 +7,9 @@ module Seko
 
     LOAD_PRODUCT_MASTER_PATH        = 'products/v4/submit'.freeze
     LOAD_PRODUCT_MASTER_UPDATE_PATH = 'products/v4/update'.freeze
-    LOAD_SALES_ORDER_PATH = 'salesorders/v4/submit'.freeze
-    LOAD_WEB_SALES_ORDER_PATH = 'salesorders/v4/websubmit'.freeze
+    LOAD_SALES_ORDER_PATH           = 'salesorders/v4/submit'.freeze
+    LOAD_WEB_SALES_ORDER_PATH       = 'salesorders/v4/websubmit'.freeze
+    RETRIEVE_STOCK_QUANTITY_PATH    = 'stock/v3/quantity'.freeze
 
     def initialize(api_key)
       @api_key = api_key
@@ -29,28 +30,49 @@ module Seko
       )
     end
 
+    # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386494/Load+Product+Masters
     def load_product_master(product)
       body = product.to_json_body
 
-      HTTParty.post(API_URL + LOAD_PRODUCT_MASTER_PATH,
+      HTTParty.post(
+        API_URL + LOAD_PRODUCT_MASTER_PATH,
         headers: {
           'Content-Type' => 'application/json',
           'Accept'       => 'application/json'
         },
         query:   { api_key: api_key },
-        body:    body)
+        body:    body
+      )
     end
 
+    # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386517/API+Load+Product+Master+Updates
     def load_product_master_update(product)
       body = product.to_json_body
 
-      HTTParty.post(API_URL + LOAD_PRODUCT_MASTER_UPDATE_PATH,
+      HTTParty.post(
+        API_URL + LOAD_PRODUCT_MASTER_UPDATE_PATH,
         headers: {
           'Content-Type' => 'application/json',
           'Accept'       => 'application/json'
         },
         query:   { api_key: api_key },
-        body:    body)
+        body:    body
+      )
+    end
+
+    # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386628/API+Retrieve+Stock+Quantity
+    def retrieve_stock_quantity(product_codes, dc_code = nil)
+      params = {
+        api_key:      api_key,
+        productCodes: product_codes.join(',')
+      }
+      params[:dc] = dc_code unless dc_code.nil?
+
+      HTTParty.post(
+        API_URL + RETRIEVE_STOCK_QUANTITY_PATH,
+        headers: { 'Accept' => 'application/json' },
+        query:   params
+      )
     end
 
     private
--- spec/seko/client_spec.rb
+++ spec/seko/client_spec.rb
@@ -609,5 +609,71 @@ module Seko
         }.to_json)
       end
     end
+
+    describe '#retrieve_stock_quantity' do
+      it 'makes request to SEKO API' do
+        stub_const('Seko::Client::API_URL', 'https://test.service.com:8081/api/')
+        stub_request(:post, "#{Seko::Client::API_URL}stock/v3/quantity")
+          .with(
+            headers: { 'Accept' => 'application/json' },
+            query: {
+              'api_key'      => 'ABC',
+              'productCodes' => 'pmtest123,pmtest234',
+              'dc'           => 'DCCL01'
+            }
+          ).to_return(
+            body: {
+              'CallStatus' => {
+                'Success' => true,
+                'Code'    => 0,
+                'Message' => ''
+              },
+              'List' => {
+                'StockQuantityLineItem' => [
+                  {
+                    'ProductCode'  => 'PMTest123',
+                    'DCCode'       => 'DCCL01',
+                    'LocationType' => 'Available Stock',
+                    'Quantity'     => 976
+                  },
+                  {
+                    'ProductCode'  => 'PMTest234',
+                    'DCCode'       => 'DCCL01',
+                    'LocationType' => 'Available Stock',
+                    'Quantity'     => 957
+                  }
+                ]
+              }
+            }.to_json
+          )
+
+        client = Client.new('ABC')
+        response = client.retrieve_stock_quantity(product_codes = ['pmtest123', 'pmtest234'], dc_code = 'DCCL01')
+
+        expect(response.parsed_response).to eq({
+          'CallStatus' => {
+            'Success' => true,
+            'Code'    => 0,
+            'Message' => ''
+          },
+          'List' => {
+            'StockQuantityLineItem' => [
+              {
+                'ProductCode'  => 'PMTest123',
+                'DCCode'       => 'DCCL01',
+                'LocationType' => 'Available Stock',
+                'Quantity'     => 976
+              },
+              {
+                'ProductCode'  => 'PMTest234',
+                'DCCode'       => 'DCCL01',
+                'LocationType' => 'Available Stock',
+                'Quantity'     => 957
+              }
+            ]
+          }
+        }.to_json)
+      end
+    end
   end
 end
