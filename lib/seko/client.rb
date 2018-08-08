a7bf3b9e9a1f37beb98886169c18b307b871f060
Irek Skrobis <irek.skrobis@gmail.com>
2018-08-08 14:09:29 +0200

    client refactoring

--- lib/seko/client.rb
+++ lib/seko/client.rb
@@ -21,61 +21,25 @@ module Seko
     # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386597/API+Load+Sales+Orders
     def load_sales_order(order)
       body = order.to_json_body
-
-      HTTParty.post(
-        api_url + LOAD_SALES_ORDER_PATH,
-        headers: {
-          'Content-Type' => 'application/json',
-          'Accept'       => 'application/json'
-        },
-        query:   { api_key: api_key },
-        body:    body
-      )
+      post_request(path: LOAD_SALES_ORDER_PATH, body: body)
     end
 
     # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386616/API+Load+Web+Sales+Orders
     def load_web_sales_order(order)
       body = order.to_json_body
-
-      HTTParty.post(
-        api_url + LOAD_WEB_SALES_ORDER_PATH,
-        headers: {
-          'Content-Type' => 'application/json',
-          'Accept'       => 'application/json'
-        },
-        query:   { api_key: api_key },
-        body:    body
-      )
+      post_request(path: LOAD_WEB_SALES_ORDER_PATH, body: body)
     end
 
     # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386504/API+Load+Product+Masters
     def load_product_master(product)
       body = product.to_json_body
-
-      HTTParty.post(
-        api_url + LOAD_PRODUCT_MASTER_PATH,
-        headers: {
-          'Content-Type' => 'application/json',
-          'Accept'       => 'application/json'
-        },
-        query:   { api_key: api_key },
-        body:    body
-      )
+      post_request(path: LOAD_PRODUCT_MASTER_PATH, body: body)
     end
 
     # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386517/API+Load+Product+Master+Updates
     def load_product_master_update(product)
       body = product.to_json_body
-
-      HTTParty.post(
-        api_url + LOAD_PRODUCT_MASTER_UPDATE_PATH,
-        headers: {
-          'Content-Type' => 'application/json',
-          'Accept'       => 'application/json'
-        },
-        query:   { api_key: api_key },
-        body:    body
-      )
+      post_request(path: LOAD_PRODUCT_MASTER_UPDATE_PATH, body: body)
     end
 
     # https://bigdigit.atlassian.net/wiki/spaces/IH2/pages/12386628/API+Retrieve+Stock+Quantity
@@ -107,10 +71,22 @@ module Seko
 
     private
 
+    attr_reader :api_key, :live
+
     def api_url
       live ? API_URL : API_TEST_URL
     end
 
-    attr_reader :api_key, :live
+    def post_request(path:, body:)
+      HTTParty.post(
+        api_url + path,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Accept'       => 'application/json'
+        },
+        query:   { api_key: api_key },
+        body:    body
+      )
+    end
   end
 end
